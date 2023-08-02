565a8964c622c876e6a1f38e8733998348a7f064
guwonstan <129030804+guwonstan@users.noreply.github.com>
2023-08-03 07:30:00 +0800

    mnt

--- MaintenanceView.java
+++ MaintenanceView.java
@@ -1,3 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
 public class MaintenanceView {
-    
+    private JPanel maintenanceMenu;
+
+    public MaintenanceView(){
+        this.maintenanceMenu = Menu();
+    }
+
+    private JPanel Menu(){
+        JPanel panel = new JPanel();
+
+        JButton restock = new JButton("Restock");
+        JButton replenishCash = new JButton("Replenish Change");
+        JButton collectMoney = new JButton("Collect Money");
+        return panel;
+    }
 }
