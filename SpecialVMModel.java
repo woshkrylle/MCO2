3c980a037b793339be067be081e7441bcec556d7
guwonstan <129030804+guwonstan@users.noreply.github.com>
2023-08-03 04:07:00 +0800

    buh

--- SpecialVMModel.java
+++ SpecialVMModel.java
@@ -1,7 +1,3 @@
-import javax.swing.JButton;
-import javax.swing.JLabel;
-
-
 public class SpecialVMModel extends VMModel {
     public SpecialVMModel(Controller controller){
         super(controller);
